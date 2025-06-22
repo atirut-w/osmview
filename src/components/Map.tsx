0d95a67acd0f3b0c576d7c957051ae3b6587d0da
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-06-22 18:33:36 +0700

    Refactor map into separate component

---
+++ src/components/Map.tsx
@@ -0,0 +1,67 @@
+import { useEffect } from 'react'
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import './Map.css'
+
+// Fix for default marker icons in react-leaflet
+import L from 'leaflet'
+import icon from 'leaflet/dist/images/marker-icon.png'
+import iconShadow from 'leaflet/dist/images/marker-shadow.png'
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41]
+})
+
+L.Marker.prototype.options.icon = DefaultIcon
+
+// Component to handle map center updates when position changes
+function MapCenterUpdater({ position }: { position: [number, number] | null }) {
+  const map = useMap()
+  
+  useEffect(() => {
+    if (position) {
+      map.flyTo(position, 13) // Zoom level 13 for better view of the area
+    }
+  }, [position, map])
+  
+  return null
+}
+
+interface MapProps {
+  position: [number, number] | null;
+  locationEnabled: boolean;
+}
+
+function Map({ position, locationEnabled }: MapProps) {
+  return (
+    <div className="map-container">
+      <MapContainer 
+        center={[0, 0]} 
+        zoom={2} 
+        style={{ height: '100%', width: '100%' }}
+        zoomControl={false} // Move zoom control to right side
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {position && locationEnabled && (
+          <Marker position={position}>
+            <Popup>
+              Your location<br />
+              Latitude: {position[0].toFixed(4)}<br />
+              Longitude: {position[1].toFixed(4)}
+            </Popup>
+          </Marker>
+        )}
+        {/* This component will update the map center when position changes */}
+        {position && locationEnabled && <MapCenterUpdater position={position} />}
+      </MapContainer>
+    </div>
+  )
+}
+
+export default Map
